69493d8ba6b5b179f5d632e0ddf8d53dde1f6f53
userf <you@example.com>
2019-10-12 16:02:57 +0300

    firs commit

---
+++ djangogirls/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from django.urls import path, include
+from django.contrib import admin
+
+from django.contrib.auth import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('', include('blog.urls')),
+]
